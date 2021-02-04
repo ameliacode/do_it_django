3ef63498174fa035a6e65456ef185af0bea5892d
ameliacode <17011663@sju.ac.kr>
2021-02-04 23:34:07 +0900

    대문 페이지와 자기소개 페이지 만들기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('blog',include("blog.urls")),
     path('admin/', admin.site.urls),
+    path('blog',include("blog.urls")),
+    path("", include("single_pages.urls")),
 ]
